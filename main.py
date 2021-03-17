fe580d7fcbd4a22d499e5e4e3332748894ec2c35
suddu99 <77456838+suddu99@users.noreply.github.com>
2021-03-17 16:22:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import pandas as pd
 # load data
 df1 = pd.read_csv("data.csv")
 
-# drop rows which have same order_id
-# and customer_id and keep latest entry
+#filter the duplicates
 newdf = df1.drop_duplicates( subset = ['Mobile Number','Service Indication'], keep="first")
 
 #copy data to other file
